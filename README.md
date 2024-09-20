5b25b84db51095db0bda66e8e431df90dfbf7161
Kamat, Trivikram <16024985+trivikr@users.noreply.github.com>
2024-09-20 09:51:43 -0700

    chore: add benchmarks for 16 to 1024 KB

--- README.md
+++ README.md
@@ -12,10 +12,31 @@ Benchmarks checksum implementations used by AWS SDK for JavaScript.
 ```console
 $ yarn bench
 
-Benchmark:
-crc32 x 89,777 ops/sec ±0.90% (95 runs sampled)
-crc32c x 91,208 ops/sec ±0.43% (95 runs sampled)
-sha1 x 552,898 ops/sec ±1.82% (82 runs sampled)
-sha256 x 554,440 ops/sec ±2.60% (74 runs sampled)
-Fastest is sha1,sha256
+Benchmark for buffer of size 16 KB:
+crc32 x 5,847 ops/sec ±0.94% (97 runs sampled)
+crc32c x 5,696 ops/sec ±2.56% (95 runs sampled)
+sha1 x 115,813 ops/sec ±3.62% (89 runs sampled)
+sha256 x 117,583 ops/sec ±2.80% (87 runs sampled)
+Fastest is sha256,sha1
+
+Benchmark for buffer of size 64 KB:
+crc32 x 1,438 ops/sec ±0.58% (97 runs sampled)
+crc32c x 1,378 ops/sec ±4.90% (93 runs sampled)
+sha1 x 33,010 ops/sec ±2.74% (91 runs sampled)
+sha256 x 34,076 ops/sec ±1.57% (91 runs sampled)
+Fastest is sha256
+
+Benchmark for buffer of size 256 KB:
+crc32 x 361 ops/sec ±1.13% (93 runs sampled)
+crc32c x 367 ops/sec ±0.43% (94 runs sampled)
+sha1 x 9,096 ops/sec ±0.43% (101 runs sampled)
+sha256 x 9,178 ops/sec ±0.58% (100 runs sampled)
+Fastest is sha256
+
+Benchmark for buffer of size 1024 KB:
+crc32 x 90.94 ops/sec ±0.36% (79 runs sampled)
+crc32c x 90.49 ops/sec ±0.47% (79 runs sampled)
+sha1 x 2,261 ops/sec ±1.45% (96 runs sampled)
+sha256 x 2,111 ops/sec ±7.35% (91 runs sampled)
+Fastest is sha1
 ```
--- src/bench.js
+++ src/bench.js
@@ -9,41 +9,43 @@ const generateBuffer = (size) => {
   return buf;
 };
 
-const suite = new benchmark.Suite();
-const testBuffer = generateBuffer(1024);
-
 const crc32 = new AwsCrc32();
 const crc32c = new AwsCrc32c();
 const sha1 = new (Hash.bind(null, "sha1"))();
 const sha256 = new (Hash.bind(null, "sha256"))();
 
-console.log(`Benchmark:`);
-suite
-  .add("crc32", () => {
-    crc32.update(testBuffer);
-    crc32.digest(16);
-    crc32.reset();
-  })
-  .add("crc32c", () => {
-    crc32c.update(testBuffer);
-    crc32c.digest(16);
-    crc32c.reset();
-  })
-  .add("sha1", () => {
-    sha1.update(testBuffer);
-    sha1.digest(16);
-    sha1.reset();
-  })
-  .add("sha256", () => {
-    sha256.update(testBuffer);
-    sha256.digest(16);
-    sha256.reset();
-  })
-  .on("cycle", (event) => {
-    console.log(String(event.target));
-  })
-  .on("complete", () => {
-    console.log("Fastest is " + suite.filter("fastest").map("name"));
-  })
-  // run sync
-  .run({ async: false });
+for (const bufferSizeInKB of [16, 64, 256, 1024]) {
+  const suite = new benchmark.Suite();
+  const testBuffer = generateBuffer(bufferSizeInKB * 1024);
+
+  console.log(`\nBenchmark for buffer of size ${bufferSizeInKB} KB:`);
+  suite
+    .add("crc32", () => {
+      crc32.update(testBuffer);
+      crc32.digest(16);
+      crc32.reset();
+    })
+    .add("crc32c", () => {
+      crc32c.update(testBuffer);
+      crc32c.digest(16);
+      crc32c.reset();
+    })
+    .add("sha1", () => {
+      sha1.update(testBuffer);
+      sha1.digest(16);
+      sha1.reset();
+    })
+    .add("sha256", () => {
+      sha256.update(testBuffer);
+      sha256.digest(16);
+      sha256.reset();
+    })
+    .on("cycle", (event) => {
+      console.log(String(event.target));
+    })
+    .on("complete", () => {
+      console.log("Fastest is " + suite.filter("fastest").map("name"));
+    })
+    // run sync
+    .run({ async: false });
+}
